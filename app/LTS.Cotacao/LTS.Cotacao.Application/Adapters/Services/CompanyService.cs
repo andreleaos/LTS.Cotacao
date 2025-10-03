1a31757cb0cdf23196026959f8303050b850cfe4
andreleaos <andreleaos@gmail.com>
2025-10-02 22:09:17 -0300

    Impl camada de app

---
+++ app/LTS.Cotacao/LTS.Cotacao.Application/Adapters/Services/CompanyService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LTS.Cotacao.Domain.Dtos;
+using LTS.Cotacao.Domain.Entities;
+using LTS.Cotacao.Domain.Ports.Repositories;
+using LTS.Cotacao.Domain.Ports.Services;
+
+namespace LTS.Cotacao.Application.Adapters.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly ICompanyRepository _repo;
+        public CompanyService(ICompanyRepository repo) => _repo = repo;
+        public Task<IEnumerable<Company>> GetAllAsync() => _repo.GetAllAsync();
+        public Task<Company?> GetByIdAsync(int id) => _repo.GetByIdAsync(id);
+        public Task<int> CreateAsync(CompanyDto c) => _repo.CreateAsync(c.ConvertToEntity());
+        public Task<bool> UpdateAsync(CompanyDto c) => _repo.UpdateAsync(c.ConvertToEntity());
+        public Task<bool> DeleteAsync(int id) => _repo.DeleteAsync(id);
+    }
+
+}
---
+++ app/LTS.Cotacao/LTS.Cotacao.Application/Adapters/Services/StockQuoteService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LTS.Cotacao.Domain.Dtos;
+using LTS.Cotacao.Domain.Entities;
+using LTS.Cotacao.Domain.Ports.Repositories;
+using LTS.Cotacao.Domain.Ports.Services;
+
+namespace LTS.Cotacao.Application.Adapters.Services
+{
+    public class StockQuoteService : IStockQuoteService
+    {
+        private readonly IStockQuoteRepository _repo;
+        public StockQuoteService(IStockQuoteRepository repo) => _repo = repo;
+
+        public Task<IEnumerable<StockQuote>> GetByCompanyAndRangeAsync(int companyId, DateOnly? start, DateOnly? end)
+        => _repo.GetByCompanyAndRangeAsync(companyId, start, end);
+
+
+        public Task<long> CreateAsync(StockQuoteDto q) => _repo.CreateAsync(q.ConvertToEntity());
+        public Task<bool> UpdateAsync(StockQuoteDto q) => _repo.UpdateAsync(q.ConvertToEntity());
+        public Task<bool> DeleteAsync(long id) => _repo.DeleteAsync(id);
+        public Task<StockQuote?> GetByIdAsync(long id) => _repo.GetByIdAsync(id);
+    }
+
+}
